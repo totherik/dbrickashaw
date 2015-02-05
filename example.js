8a288db3f3ae6566dc042fcf4cf606fb819f54b9
Erik Toth <totherik@gmail.com>
2015-02-05 11:06:33 -0800

    Make debuglog info more consumable and update example.

--- example.js
+++ example.js
@@ -3,7 +3,17 @@ import Dbrickashaw from './dist/index';
 // This would be part of the published Module API.
 export const publisher = Dbrickashaw.getPublisher();
 publisher.on('log', function ({ source, ts, tags, data }) {
-    console.log(source, tags, ts, data);
+    console.log('', ts, source, tags.join(','), data);
+});
+
+
+//C onsumers can also filter events...
+let errors = publisher.filter(({ tags }) => tags.error );
+errors.on('log', function ({ source, ts, tags, data }) {
+    if (data instanceof Error) {
+        data = data.stack;
+    }
+    console.log('', ts, source, tags.join(','), 'WHOA!!', data);
 });
 
 
@@ -11,3 +21,4 @@ publisher.on('log', function ({ source, ts, tags, data }) {
 let logger = Dbrickashaw.createLogger();
 logger.log('bar', 'foo');
 logger.info(['bar', 'foo'], { bar: 'bar'});
+logger.error(null, new Error('FAILURE'));
