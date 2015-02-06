3185d986099fcfedc2d1d37b04bbb29e075b7dff
Erik Toth <totherik@gmail.com>
2015-02-06 10:04:59 -0800

    Experimental module instrumentation.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import caller from 'caller';
+import Thing from 'core-util-is';
 import Publisher from './lib/publisher';
 import Logger from './lib/logger';
 
@@ -6,6 +7,22 @@ import Logger from './lib/logger';
 const PUBLISHER = new Publisher();
 
 export default {
+
+    instrument(parent) {
+        if (Thing.isPrimitive(parent.exports)) {
+            return;
+        }
+
+        // Here we use a microtask so this can be put anywhere in
+        // the file of the module being decorated and still operate
+        // on the exported object.
+        setImmediate(() => {
+            // Assign with default values such that it's non-enumerable, etc.
+            Object.defineProperty(parent.exports, 'publisher', {
+                value: this.getPublisher()
+            });
+        });
+    },
 
     getPublisher() {
         return PUBLISHER;
